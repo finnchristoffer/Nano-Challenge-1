ed4ee03a45d9229387fa4100bc54e7ec77a9f75f
finnchristoffer <finn.christoffer@gmail.com>
2022-04-29 08:42:00 +0700

    Initial Commit

---
+++ NanoChallenge1/NanoChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge1App.swift
+//  NanoChallenge1
+//
+//  Created by Finn Christoffer Kurniawan on 29/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
